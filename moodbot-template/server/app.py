d33588e86857f94aade42f9f3fe8a3b5c93ba234
tejash4444 <tejashpoddarvis@gmail.com>
2025-04-14 12:11:46 +0530

    Delete moodbot-template directory

--- moodbot-template/server/app.py
+++
@@ -1,6 +0,0 @@
-from app import create_app  # this 'app' refers to the folder named 'app/'
-
-app = create_app()
-
-if __name__ == "__main__":
-    app.run(debug=True)
--- moodbot-template/server/app/users.py
+++
@@ -1,3 +0,0 @@
-users = {
-    "user1": {"coins": 100, "last_daily_reward": None}
-}
